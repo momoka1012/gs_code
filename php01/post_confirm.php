90f407a292e9c212289c89ccccc2867593110e43
Unknown <kano-hano-2114@softbank.ne.jp>
2020-05-29 12:15:23 +0900

    初回登録

---
+++ php01/post_confirm.php
@@ -0,0 +1,46 @@
+<?php
+function h($value){
+    return htmlspecialchars($value,ENT_QUOTES);
+
+}
+
+$flg = 0;
+$name = $_POST["name"];
+$mail = $_POST["mail"];
+
+$ary = array("$name","$mail");
+$str = implode(',',$ary);
+
+var_dump($str);
+
+
+
+//File書き込み
+//名前とメールの変数をカンマ区切りの文字列にしてファイルに書き込めるようにする。
+$file = fopen("data/data.txt","a");
+fwrite($file,$str."\r\n");
+fclose($file);
+
+
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>POST（受信）</title>
+</head>
+<body>
+<!--お名前：<?php echo h($name); ?>
+EMAIL：<?php echo h($mail); ?> -->
+
+<?php
+     if($flg == 0){
+?>
+    <button>登録</button>
+     <?php
+     }
+     ?>
+<ul>
+<li><a href="index.php">index.php</a></li>
+</ul>
+</body>
+</html>
